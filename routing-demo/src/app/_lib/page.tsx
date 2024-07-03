84fea6fa576e9c783ef0db40d8d8977dec8f053c
ShankalpaPokharel <pokharelshankalpa58@gmail.com>
2024-07-03 17:21:31 +0545

    File colocation and Layouts

---
+++ routing-demo/src/app/_lib/page.tsx
@@ -0,0 +1,5 @@
+export default function PrivateRoute() {
+  return (
+    <div>you can't view this in the browser</div>
+  )
+}
---
+++ routing-demo/src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+function BarChart (){
+    return <h1>Barchart</h1>
+}
+
+export default function Dashboard() {
+  return (
+    <div>Dashboard</div>
+  )
+}
